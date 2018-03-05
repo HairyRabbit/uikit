ff855af7e1d91130c161b9aa60c5728831c9e405
rabbit <yfhj1990@hotmail.com>
2018-03-05 08:13:30 +0800

    fix webpack function config with webpack-serve

--- webpack.config.js
+++ webpack.config.js
@@ -6,45 +6,51 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 const env = isProd ? 'production' : 'development'
+const name = process.env.UIKITPKG
 
-module.exports = function({ name }) {
-  const moduleName = kebabCase(name)
-  return {
-    mode: env,
-    entry: isProd ? './index.js' : './boot.js',
-    output: {
-      path: path.resolve('lib'),
-      filename: moduleName + '.js',
-      library: name,
-      libraryTarget: 'umd'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, use: 'babel-loader' },
-        { test: /\.css$/, use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: {
-              modules: true,
-              importLoader: 1,
-              minimize: isProd,
-              localIdentName: isProd ? '[hash:base64:8]' : '[path][name]__[local]--[hash:base64:5]'
-            }},
-            'postcss-loader'
-          ]
-        })}
-      ]
-    },
-    externals: isProd ? {
-      'react': 'React'
-    } : {},
-    plugins: [].concat(
-      !isProd ? new HtmlWebpackPlugin({
-        inject: false,
-        template: HtmlWebpackTemplate,
-        appMountId: 'app'
-      }) : [],
-      new ExtractTextPlugin(moduleName + '.css')
-    )
-  }
+if(!name) {
+  throw new Error('No UIKITPKG')
+}
+
+const moduleName = kebabCase(name)
+
+module.exports = {
+  mode: env,
+  entry: isProd ? './index.js' : './boot.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: moduleName + '.js',
+    library: name,
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          { loader: 'css-loader', options: {
+            modules: true,
+            importLoader: 1,
+            minimize: isProd,
+            localIdentName: isProd
+              ? '[hash:base64:8]'
+              : '[path][name]__[local]--[hash:base64:5]'
+          }},
+          'postcss-loader'
+        ]
+      })}
+    ]
+  },
+  externals: isProd ? {
+    'react': 'React'
+  } : {},
+  plugins: [].concat(
+    !isProd ? new HtmlWebpackPlugin({
+      inject: false,
+      template: HtmlWebpackTemplate,
+      appMountId: 'app'
+    }) : [],
+    new ExtractTextPlugin(moduleName + '.css')
+  )
 }
