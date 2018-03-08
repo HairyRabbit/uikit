a77e61767aebda3a126ea61975dc4b1d8bce12cb
rabbit <yfhj1990@hotmail.com>
2018-03-08 14:37:36 +0800

    setup spa failback.

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: false,
       template: HtmlWebpackTemplate,
-      appMountId: 'app'
+      appMountId: 'app',
+      headHtmlSnippet: `
+<script>
+      (function(l) {
+        if (l.search) {
+          var q = {};
+          l.search.slice(1).split('&').forEach(function(v) {
+            var a = v.split('=');
+            q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+          });
+          if (q.p !== undefined) {
+            window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + (q.p || '') +
+              (q.q ? ('?' + q.q) : '') +
+              l.hash
+            );
+          }
+        }
+      }(window.location))
+</script>`
     }),
     new HtmlWebpackPlugin({
       inject: false,
